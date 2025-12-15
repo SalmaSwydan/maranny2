9edadbc78c4bc5ff7140228eb0232d61a1f31e52
SalmaSwydan <salma.swydan.ss@gmail.com>
2025-12-15 16:30:38 +0200

    Add app assets constants

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,51 @@
+class AppAssets {
+  AppAssets._();
+
+  // ================= Images =================
+  static const String backgroundScreen =
+      'assets/images/background_screen.png';
+
+  static const String marannyLogo =
+      'assets/images/maranny_logo.png';
+
+  // ================= Bottom Nav Icons =================
+  static const String homeIcon =
+      'assets/icons/home_icon.svg';
+
+  static const String homeIconSelected =
+      'assets/icons/home_icon_selected.svg';
+
+  static const String bookingsIcon =
+      'assets/icons/booking_icon.svg';
+
+  static const String bookingsIconSelected =
+      'assets/icons/bookings_icon_selected.svg';
+
+  static const String messagesIcon =
+      'assets/icons/message_icon.svg';
+
+  static const String messagesIconSelected =
+      'assets/icons/messages_icon_selected.svg';
+
+  static const String profileIcon =
+      'assets/icons/profile_icons.svg';
+
+  static const String profileIconSelected =
+      'assets/icons/profile_icon_selected.svg';
+
+  static const String marketplaceIconSelected =
+      'assets/icons/marketplace_icon_selected.svg';
+
+  // ================= Other Icons =================
+  static const String locationIcon =
+      'assets/icons/location_icon.svg';
+
+  static const String recommendationIcon =
+      'assets/icons/recommendation_icon.svg';
+
+  static const String userReviewIcon =
+      'assets/icons/userReview_icon.svg';
+
+  static const String rightArrowIcon =
+      'assets/icons/right_icon.svg';
+}
